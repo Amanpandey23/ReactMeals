911226174d18e1469740fbf6ff819ca1336afe50
Aman <pande23yaman@gmail.com>
2022-03-16 18:55:00 +0530

    Added some css and functionality to add to cart items and removal of cart items

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,6 +1,6 @@
-import classes from "./AvailableMeals.module.css";
-import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
   {
@@ -33,10 +33,10 @@ const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
-      m
     />
   ));
 
